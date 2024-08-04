15c07b399d590ed845024615812a6fc487da13a6
VedanshPaliwal88 <66903973+VedanshPaliwal88@users.noreply.github.com>
2024-08-04 13:14:02 +0530

    added new project adding functionality

--- src/modules/populateMenu.js
+++ src/modules/populateMenu.js
@@ -1,4 +1,5 @@
 import populateContent from "./populateContent";
+import project from "./project";
 
 export default function populateMenu(myProjects) {
     let menu = document.querySelector("menu");
@@ -30,7 +31,28 @@ export default function populateMenu(myProjects) {
     let newProjectBtn = document.createElement('div');
     newProjectBtn.classList.add('add-project');
     newProjectBtn.textContent = "+ Add Project";
-    // add event
+    newProjectBtn.addEventListener('click', () => {
+        let newProjectDiv = document.createElement('div');
+        let newProjectInput = document.createElement('input');
+        let submtiBtn = document.createElement('button');
+
+        newProjectInput.type = 'text';
+        newProjectInput.classList.add('newProjectInput')
+        newProjectInput.autofocus = true;
+        submtiBtn.textContent = "add";
+        
+        submtiBtn.addEventListener('click', () => {
+            if (newProjectInput.value) {
+                let newProject = new project(newProjectInput.value);
+                myProjects.push(newProject);
+                populateMenu(myProjects);
+            }
+        })
+        newProjectDiv.append(newProjectInput, submtiBtn);
+
+        menuContainer.insertBefore(newProjectDiv, newProjectBtn);
+
+    })
     menuContainer.appendChild(newProjectBtn);
 
     menu.appendChild(menuContainer);

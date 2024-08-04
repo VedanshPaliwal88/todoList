3bb87e7e236af46dca653d577d3397219afb20ff
VedanshPaliwal88 <66903973+VedanshPaliwal88@users.noreply.github.com>
2024-08-04 13:23:01 +0530

    fixed bug when there are no projects to display

--- src/modules/populateContent.js
+++ src/modules/populateContent.js
@@ -5,31 +5,34 @@ import { editItemDialog } from "./makeDialog";
 export default function populateContent(project) {
     let content = document.querySelector("#content");
     content.innerHTML = '';
-    let contentHeading = document.createElement('div');
-    let contentTitle = document.createElement('div');
-    let addItem = document.createElement('div');
 
-    contentHeading.classList.add('contentHeading');
-    contentTitle.textContent = project.name;
-    contentTitle.classList.add('contentTitle');
-    addItem.textContent = "+";
-    addItem.classList.add('addItem');
-    addItem.addEventListener('click', () => {
-        let newItem = new todoItem('', '', '', '', '');
-        project.addItem(newItem);
-        editItemDialog(newItem, project);
-        populateContent(project);
-    })
-
-    contentHeading.append(contentTitle, addItem);
-    
-    content.appendChild(contentHeading);
-
-    for (const item of project.items) {
-        if (item instanceof todoItem) {
-            content.appendChild(getTodoItem(item, project));
-        } else {
-            console.log("populate content error: type of item: ", typeof(item));
+    if(project) {
+        let contentHeading = document.createElement('div');
+        let contentTitle = document.createElement('div');
+        let addItem = document.createElement('div');
+        
+        contentHeading.classList.add('contentHeading');
+        contentTitle.textContent = project.name;
+        contentTitle.classList.add('contentTitle');
+        addItem.textContent = "+";
+        addItem.classList.add('addItem');
+        addItem.addEventListener('click', () => {
+            let newItem = new todoItem('', '', '', '', '');
+            project.addItem(newItem);
+            editItemDialog(newItem, project);
+            populateContent(project);
+        })
+        
+        contentHeading.append(contentTitle, addItem);
+        
+        content.appendChild(contentHeading);
+        
+        for (const item of project.items) {
+            if (item instanceof todoItem) {
+                content.appendChild(getTodoItem(item, project));
+            } else {
+                console.log("populate content error: type of item: ", typeof(item));
+            }
         }
     }
 }
